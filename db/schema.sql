fe2689304c25dc6d847cae83743a394018d65322
Steve Calla <callasteven@gmail.com>
2022-09-13 08:42:16 -0600

    Initial commit. Add sequalize sync on server js

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- SOURCE ./db/schema.sql
+
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = require('./routes');
+
+// import sequelize connection
+const sequelize = require('./config/connection'); //SECTION
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+// turn on connection to db and server
+//SECTION
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+
+
